d40fd66b4f00d06066b7fd89d7c5554b878d23a5
umakarki <uma88.it@gmail.com>
2022-11-28 10:44:45 +0530

    NewChanges

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiServiceService } from '../api-service.service';
@@ -13,75 +13,75 @@ import { ApiServiceService } from '../api-service.service';
 export class SignInComponent implements OnInit {
   hide = true;
 
-  signinForm=new FormGroup({
-    emailId:new FormControl('',[Validators.required,Validators.pattern("^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-z]{2,4}$")]),
-    password:new FormControl('',[Validators.required,Validators.minLength(6), Validators.maxLength(20),Validators.pattern("^[a-zA-Z.@_0-9]+$")])
+  signinForm = new FormGroup({
+    emailId: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern("^[a-zA-Z.@_0-9]+$")])
   })
-  get email(){
+  get email() {
     return this.signinForm.get('emailId')
   }
-  get password(){
+  get password() {
     return this.signinForm.get('password')
   }
-  
-    constructor(private http:HttpClient,private router:Router,private snackBar:MatSnackBar,private api:ApiServiceService) { }
-  
+
+  constructor(private http: HttpClient, private router: Router, private snackBar: MatSnackBar, private api: ApiServiceService) { }
+
 
   ngOnInit(): void {
   }
 
-  signIn(){
-   
+  signIn() {
+
     this.http.get<any>("http://localhost:3000/signupUsers")
-    .subscribe(res=>{
-      const user=res.find((a:any)=>{
-        console.log(a)
-        console.log(a.emailId);
-        console.log(a.password);
-        console.log(this.signinForm.value.emailId);
-        console.log(this.signinForm.value.password);
-        return a.emailId===this.signinForm.value.emailId && a.password===this.signinForm.value.password;
-      });
-      const user1=res.find((a:any)=>{
-        return a.emailId===this.signinForm.value.emailId && a.password!=this.signinForm.value.password;
-      });
-
-      if(user){
-
-        const logSucces="Login Success!"
-        const action="close"
-        this.snackBar.open(logSucces,action,{
-          duration:5000,
-          verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
-          horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+      .subscribe(res => {
+        const user = res.find((a: any) => {
+          console.log(a)
+          console.log(a.emailId);
+          console.log(a.password);
+          console.log(this.signinForm.value.emailId);
+          console.log(this.signinForm.value.password);
+          return a.emailId === this.signinForm.value.emailId && a.password === this.signinForm.value.password;
+        });
+        const user1 = res.find((a: any) => {
+          return a.emailId === this.signinForm.value.emailId && a.password != this.signinForm.value.password;
         });
 
-        this.router.navigate(['dashboard/sip'])
-      }else if(user1){
+        if (user) {
 
-        const pswrdNotmatch="Password does not match!!"
-        const action="close"
-        this.snackBar.open(pswrdNotmatch,action,{
-          duration:5000,
-          verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
-          horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
-        });
-        
-      }else{
+          const logSucces = "Login Success!"
+          const action = "close"
+          this.snackBar.open(logSucces, action, {
+            duration: 5000,
+            verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
+            horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+          });
 
-        const userNtFound="User not found!!"
-        const action="close"
-        this.snackBar.open(userNtFound,action,{
-          duration:5000,
+          this.router.navigate(['dashboard/sip'])
+        } else if (user1) {
+
+          const pswrdNotmatch = "Password does not match!!"
+          const action = "close"
+          this.snackBar.open(pswrdNotmatch, action, {
+            duration: 5000,
+            verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
+            horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+          });
+
+        } else {
+
+          const userNtFound = "User not found!!"
+          const action = "close"
+          this.snackBar.open(userNtFound, action, {
+            duration: 5000,
+          });
+        }
+      }, err => {
+        const wentWrong = "Something went wrong!!"
+        const action = "close"
+        this.snackBar.open(wentWrong, action, {
+          duration: 5000,
         });
-      }
-    },err=>{
-      const wentWrong="Something went wrong!!"
-      const action="close"
-      this.snackBar.open(wentWrong,action,{
-        duration:5000,
-      });
-    })
+      })
   }
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -13,65 +13,65 @@ export class SignUpComponent implements OnInit {
 
   hide = true;
 
-  signupForm=new FormGroup({
-  firstName:new FormControl('',[Validators.required,Validators.minLength(3), Validators.maxLength(20),Validators.pattern("^[a-zA-Z.]+$")]),
-  lastName:new FormControl('',[Validators.required,Validators.minLength(3), Validators.maxLength(20),Validators.pattern("^[a-zA-Z.]+$")]),
-  mobileNumber:new FormControl('',[Validators.required,Validators.pattern(/^((9|8|7|0)[0-9]{9})$/)]),
-  emailId:new FormControl('',[Validators.required,Validators.pattern("^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-z]{2,4}$")]),
-  city:new FormControl('',[Validators.required,Validators.minLength(3), Validators.maxLength(20),Validators.pattern("^[a-zA-Z.]+$")]),
-  password:new FormControl('',[Validators.required,Validators.minLength(6), Validators.maxLength(20),Validators.pattern("^[a-zA-Z.@_0-9]+$")]),
-})
-get firstName(){
- return this.signupForm.get('firstName');
-}
-get lastName(){
-  return this.signupForm.get('lastName');
- }
- get mobileNumber(){
-  return this.signupForm.get('mobileNumber');
- }
- get email(){
-  return this.signupForm.get('emailId');
- }
- 
- get city(){
-  return this.signupForm.get('city');
- }
- 
- get passWord(){
-  return this.signupForm.get('password');
- }
- 
-  constructor(private router:Router,private http:HttpClient, private snackBar:MatSnackBar) { }
+  signupForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z.]+$")]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z.]+$")]),
+    mobileNumber: new FormControl('', [Validators.required, Validators.pattern(/^((9|8|7|0)[0-9]{9})$/)]),
+    emailId: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-z]{2,4}$")]),
+    city: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z.]+$")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern("^[a-zA-Z.@_0-9]+$")]),
+  })
+  get firstName() {
+    return this.signupForm.get('firstName');
+  }
+  get lastName() {
+    return this.signupForm.get('lastName');
+  }
+  get mobileNumber() {
+    return this.signupForm.get('mobileNumber');
+  }
+  get email() {
+    return this.signupForm.get('emailId');
+  }
+
+  get city() {
+    return this.signupForm.get('city');
+  }
+
+  get passWord() {
+    return this.signupForm.get('password');
+  }
+
+  constructor(private router: Router, private http: HttpClient, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
-  signUp(){
+  signUp() {
     console.log(this.signupForm.value)
-    
 
-    this.http.post<any>("http://localhost:3000/signupUsers",this.signupForm.value)
-    .subscribe(res=>{
-      // console.log(res)
-      const sucessMeg="signUp Sucessfully";
-      const action="close";
-      this.snackBar.open(sucessMeg,action, {
-        duration:100000,
-        verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
+
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
+      .subscribe(res => {
+        // console.log(res)
+        const sucessMeg = "signUp Sucessfully";
+        const action = "close";
+        this.snackBar.open(sucessMeg, action, {
+          duration: 100000,
+          verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
           horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
 
-      });
+        });
 
-      this.signupForm.reset();
-      this.router.navigate(['signin'])
-    },err=>{
-      const wentWrong="Something went wrong";
-      const action="close";
-      this.snackBar.open(wentWrong,action, {
-        duration:3000,
-      });
-    })
-    
+        this.signupForm.reset();
+        this.router.navigate(['signin'])
+      }, err => {
+        const wentWrong = "Something went wrong";
+        const action = "close";
+        this.snackBar.open(wentWrong, action, {
+          duration: 3000,
+        });
+      })
+
   }
 
 }
